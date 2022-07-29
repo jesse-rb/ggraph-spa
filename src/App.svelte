cfe11882a994c9539ddfc64887eced70515df789
jesse-rb <jesse.reynekebarnard@gmail.com>
2022-07-29 23:27:44 +1200

    Initial commit -- push source to remote

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+	let path = '';
+	let min = -10;
+	let max = 10;
+	let slider = 1;
+	$: {
+		path = '';
+		for (let i = min; i <= max; i++) {
+			let x = i;
+			let y = x*x;
+			path += (i==min ? 'M' : 'L')+x+' '+(-y)+ ' ';
+		}
+		path += 'Z';
+	}
+
+	function range(n, m) {
+		let arr = [];
+		for (let i = n; i <= m; i++) {
+			arr.push(i);
+		}
+		return arr;
+	}
+</script>
+
+<input type="range" bind:value={slider} min="1" max="500">
+
+<div class="graph-container">
+	<svg viewBox="{min} {min*max} {max*2} {max*max*2}" class="graph">
+		<path d={path} fill="green" />
+		<circle cx="0" cy="0" r="1%" fill="red" />
+		{#each range(min, max*2) as i}
+			<text x="{i*max}" y="10" fill="orange" class="small">{i}</text>
+			<text x="5" y="{i*max}" fill="orange" class="small">{-i}</text>
+		{/each}
+	</svg>
+</div>
+
+<style>
+	.graph-container {
+		width: 500px;
+		height: 500px;
+	}
+	.graph {
+		display: block;
+		background-color: #2e2e2e;
+		width: 100%;
+		height: 100%;
+	}
+	.graph .small {
+		font-size: 5px;
+	}
+</style>
