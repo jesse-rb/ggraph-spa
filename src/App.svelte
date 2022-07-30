95d7dab3a036fbe50534e9d30da98fe30264e2a8
jesse-rb <jesse.reynekebarnard@gmail.com>
2022-07-30 12:55:53 +1200

    Add more sliders for x and y offset

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,13 @@
 <script>
 	let path = '';
-	let slider = 1;
+	
 	let min = -10;
 	let max = 10;
+
+	let scale = 1;
+	let xOffset = 0;
+	let yOffset = 0;
+
 	$: {
 		path = '';
 		for (let i = min; i <= max; i++) {
@@ -10,8 +15,12 @@
 			let y = x*x;
 
 			// scale
-			x = x*slider
-			y = y*slider
+			x = x*scale;
+			y = y*scale;
+
+			// Offset
+			x = x-(-1*xOffset);
+			y = y-(-1*yOffset);
 
 			path += (i==min ? 'M' : 'L')+x+' '+(-y)+ ' ';
 		}
@@ -27,15 +36,24 @@
 	}
 </script>
 
-<input type="range" bind:value={slider} min="-100" max="100">
+<label for="scale">scale</label>
+<input id="scale" type="range" bind:value={scale} min={min} max={max}>
+
+<label for="xOffset">xOffset</label>
+<input id="xOffset" type="range" bind:value={xOffset} min={min} max={max}>
+
+<label for="yOffset">yOffset</label>
+<input id="yOffset" type="range" bind:value={yOffset} min={min} max={max}>
 
 <div class="graph-container">
-	<svg viewBox="{min} {min*max} {max*2} {max*max*2}" class="graph">
+	<svg viewBox="{min} {min} {max*2} {max*2}" class="graph">
 		<path d={path} fill="green" />
+		<!--center dot-->
 		<circle cx="0" cy="0" r="1%" fill="red" />
-		{#each range(min, max*2) as i}
-			<text x="{i*max}" y="10" fill="orange" class="small">{i}</text>
-			<text x="5" y="{i*max}" fill="orange" class="small">{-i}</text>
+		<!--ranges-->
+		{#each range(min, max) as i}
+			<text x="{i}" y="0" fill="orange" class="small">{i}</text> <!--x-->
+			<text x="0" y="{i}" fill="orange" class="small">{-i}</text> <!--y-->
 		{/each}
 	</svg>
 </div>
@@ -52,6 +70,6 @@
 		height: 100%;
 	}
 	.graph .small {
-		font-size: 5px;
+		font-size: 0.5px;
 	}
 </style>
